8c468600eebdd573cd3d923a40f56b9432cb2031
mmolakaha-debug <mmolakaha@gmail.com>
2026-09-20 19:54:07 +0330

    Refresh cache for smart calendar

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    هر بار که فایل‌ها را عوض کردی، فقط شماره CACHE را یکی زیاد کن
    تا نسخه جدید برای کاربر لود شود. */
 
-const CACHE = "roadmap-v15";
+const CACHE = "roadmap-v16";
 
 const FILES = [
   "./",
