be3b0838a563596e8e5d15d5acba0cf32aef8892
mmolakaha-debug <mmolakaha@gmail.com>
2026-09-23 17:09:42 +0330

    Refresh Android app cache

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    هر بار که فایل‌ها را عوض کردی، فقط شماره CACHE را یکی زیاد کن
    تا نسخه جدید برای کاربر لود شود. */
 
-const CACHE = "roadmap-v18";
+const CACHE = "roadmap-v19";
 
 const FILES = [
   "./",
